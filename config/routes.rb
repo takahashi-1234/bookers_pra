1f7ecbd91e839adb633a5b4b08513e8cf488d2c8
takahashi-1234 <riki.1130@icloud.com>
2022-02-09 06:31:16 +0000

    [update]index画面

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources:books
+  root to: "books#index"
+  resources:books,only:[:create,:destroy,:show]
 end
