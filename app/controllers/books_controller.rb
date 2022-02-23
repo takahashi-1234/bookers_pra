cc1937d9258b4b267ff65569a2306eba51bc84f6
takahashi-1234 <riki.1130@icloud.com>
2022-02-23 05:21:50 +0000

    [add]編集機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -21,6 +21,16 @@ class BooksController < ApplicationController
     @comment=Comment.new
   end
   
+  def edit
+    @book=Book.find(params[:id])
+  end
+  
+  def update
+    book=Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title,:body,:image,:user_id)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "books#index"
-  resources:books,only:[:create,:destroy,:show] do
+  resources:books,only:[:create,:destroy,:show,:edit,:update] do
     resource:favorites,only:[:create,:destroy]
     resource:comments,only:[:create,:destroy]
   end
